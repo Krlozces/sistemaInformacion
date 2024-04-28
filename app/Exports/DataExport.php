1d4601399fc50cf2f51cca429d3ba852680f7a35
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-04-27 21:30:27 -0500

    Filed the contains the data

--- app/Exports/DataExport.php
+++ app/Exports/DataExport.php
@@ -2,9 +2,12 @@
 
 namespace App\Exports;
 
+use App\Models\Persona;
+use App\Models\Personal;
 use App\Models\Registro;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
 
 class DataExport implements FromCollection, WithHeadings
 {
@@ -13,7 +16,27 @@ class DataExport implements FromCollection, WithHeadings
     */
     public function collection()
     {
-        return Registro::all();
+        $personalProcesamiento = Personal::where('area_perteneciente', 'areapro')
+        ->with('Persona')
+        ->get();
+
+        $elementos = Persona::join('intervenidos', 'intervenidos.persona_id', '=', 'personas.id')
+        ->select('dni', 'nombre', 'apellido_paterno', 'apellido_materno')
+        ->get();
+
+        $registro = Registro::select('registros.id', 'procedencia', 'registros.id', 'procedencia', 'procedencia', 
+        DB::raw('DATE(fecha_hora_infraccion) as fecha_infraccion'), 
+        DB::raw('TIME(fecha_hora_infraccion) as hora_infraccion'), 
+        DB::raw('DATE(fecha_hora_extraccion) as fecha_extraccion'), 
+        DB::raw('TIME(fecha_hora_extraccion) as hora_extraccion'),
+        DB::raw('TIMESTAMPDIFF(MINUTE, fecha_hora_infraccion, fecha_hora_extraccion) as tiempo_transcurrido_minutos'),
+        'resultado_cuantitativo', 'motivo', 'edad')
+            ->join('comisarias', 'comisarias.id', '=', 'registros.comisaria_id')
+            ->join('muestras', 'muestras.id', '=', 'registros.muestra_id')
+            ->join('intervenidos', 'intervenidos.id', '=', 'registros.intervenido_id')
+            ->get();
+
+        return $registro->merge($elementos);
     }
 
     public function headings(): array{
