cf0a6f9e9a07383fef6d1ad35648a3aeb1f3f276
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-01 17:30:41 -0500

    El campo grado se arregló y mostró correctamente

--- resources/views/certificado.blade.php
+++ resources/views/certificado.blade.php
@@ -247,7 +247,7 @@
                         @if ($elementos->description == 'SIN MUESTRA')
                             <div style="margin:0 auto; height:1px; width:120px; border-top:1px solid black;"></div>                           
                         @else
-                            {{ $personalProcesamiento->grado }}
+                            {{ $personalProcesamiento->grado->grado }}
                         @endif
                     </td>
                     <td>DNI N°</td>
