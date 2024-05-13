ede62c51c00ed275fea1f60308a6d4e277b3c13a
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-13 12:31:26 -0500

    Return conclusiones

--- app/Http/Controllers/ViewsController.php
+++ app/Http/Controllers/ViewsController.php
@@ -108,7 +108,7 @@ class ViewsController extends Controller
     }
 
     public function generarPdf($dni){
-        $elementos = Persona::select('dni', 'nombre', 'apellido_paterno', 'apellido_materno', 'nacionalidad', 'edad', 'placa', 'vehiculo', 'clase', 'recepcion_doc_referencia', 'fecha_hora_infraccion', 'fecha', 'hora', 'fecha_hora_extraccion', 'motivo', 'procedencia', 'persona', 'muestras.observaciones', 'muestras.descripcion AS description', 'licencia', 'categoria', 'resultado_cualitativo', 'resultado_cuantitativo', 'extractor', 'procesador', 'metodos.descripcion', 'sexo', 'incurso', 'numero_oficio')
+        $elementos = Persona::select('dni', 'nombre', 'apellido_paterno', 'apellido_materno', 'nacionalidad', 'edad', 'placa', 'vehiculo', 'clase', 'recepcion_doc_referencia', 'fecha_hora_infraccion', 'fecha', 'hora', 'fecha_hora_extraccion', 'motivo', 'procedencia', 'persona', 'muestras.observaciones', 'muestras.descripcion AS description', 'licencia', 'categoria', 'resultado_cualitativo', 'resultado_cuantitativo', 'extractor', 'procesador', 'metodos.descripcion', 'sexo', 'incurso', 'numero_oficio', 'conclusiones')
         ->join('intervenidos', 'intervenidos.persona_id', '=', 'personas.id')
         ->join('licencias', 'intervenidos.id', '=', 'licencias.intervenido_id')
         ->join('clases', 'licencias.clase_id', '=', 'clases.id')
