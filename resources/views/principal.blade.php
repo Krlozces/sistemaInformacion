84d771f13f614b2369a5ac167b6d59dfaf0d41ed
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-14 16:51:35 -0500

    Modal added to the view

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -27,7 +27,7 @@
                     <h3 class="expandable">{{ Auth::user()->name }}</h3>
                     <ul class="submenu">
                         <li id="openModalBtn"><a href="#"><i class="fa-solid fa-users-viewfinder"></i> Cambiar foto</a></li>
-                        <li><a href="#"><i class="fa-solid fa-gear"></i> Configurar</a></li>
+                        <li id="openModalPassword"><a href="#"><i class="fa-solid fa-gear"></i> Configurar</a></li>
                     </ul>
                 </nav>
                 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -204,6 +204,42 @@
         </form>
     </div>
     
+    <div id="myModalPassword" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <!-- Modal header -->
+            <div class="direccion">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Cambiar contraseña
+                </h3>
+                <span class="close" id="closePassword">&times;</span>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5">
+                <form method="POST" class="space-y-4" action="{{ route('cambiar-password', ['id' => Auth::user()->id]) }}">
+                    @csrf
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value={{ Auth::user()->email }} readonly />
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña actual</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    </div>
+                    <div>
+                        <label for="newPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nueva contraseña</label>
+                        <input type="password" name="newPassword" id="newPassword" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    </div>
+                    <div>
+                        <label for="confirmedPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmar contraseña</label>
+                        <input type="password" name="confirmedPassword" id="confirmedPassword" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    </div>
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cambiar contraseña</button>    
+                </form>
+            </div>
+        </div>
+    </div>
+
     <!--	--------------->
     <input type="checkbox" id="btn-menu">
     <div class="container-menu">
@@ -225,13 +261,17 @@
             <label for="btn-menu"><i class="fa-solid fa-list"></i></label>
         </div>
     </div>
-    <script>
+    <script async defer>
         document.addEventListener("DOMContentLoaded", function () {
             // Obtener el modal y el botón para abrirlo
             var modal = document.getElementById("myModal");
             var btn = document.getElementById("openModalBtn");
             var span = document.getElementsByClassName("close")[0];
             var mainImage = document.getElementById("mainImage");
+
+            var modalPassword = document.getElementById("myModalPassword");
+            var btnPassword = document.getElementById("openModalPassword");
+            var closePassword = document.getElementById("closePassword");
 
             var savedImage = sessionStorage.getItem("selectedImage");
             if (savedImage) {
@@ -242,16 +282,25 @@
             btn.onclick = function () {
                 modal.style.display = "block";
             }
+
+            btnPassword.onclick=() => {
+                modalPassword.style.display = "block";
+            }
     
             // Cerrar el modal al hacer clic en la "x"
             span.onclick = function () {
                 modal.style.display = "none";
             }
+
+            closePassword.onclick = () => {
+                modalPassword.style.display = "none";
+            }
     
             // Cerrar el modal al hacer clic fuera del contenido del modal
             window.onclick = function (event) {
-                if (event.target == modal) {
+                if (event.target == modal || event.target == modalPassword) {
                     modal.style.display = "none";
+                    modalPassword.style.display = "none";
                 }
             }
     
@@ -284,8 +333,6 @@
                     previewImage.src = "";
                     mainImage.src = data.imageUrl;
                     sessionStorage.setItem("selectedImage", data.imageUrl);
-                    
-                    
                 })
                 .catch(error => {
                     console.error("Error al guardar la imagen:", error);
