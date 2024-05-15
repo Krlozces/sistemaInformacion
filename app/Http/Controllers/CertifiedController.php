f7dc1460ec346f4c062c34a4fc04d0513f29435d
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-14 23:06:50 -0500

    Controlador para exportar el certificado a excel created

--- app/Http/Controllers/CertifiedController.php
+++ app/Http/Controllers/CertifiedController.php
@@ -9,6 +9,6 @@ use Maatwebsite\Excel\Facades\Excel;
 class CertifiedController extends Controller
 {
     public function exportCertified(){
-        return Excel::download(new CertificadoExport, 'certificado.xlsx')
+        return Excel::download(new CertificadoExport, 'certificado.xlsx');
     }
 }
