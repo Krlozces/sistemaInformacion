16f8373d6086c997fcc14cddf39c137d529f859a
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-16 18:47:56 -0500

    Name formatted

--- app/Http/Controllers/CertifiedController.php
+++ app/Http/Controllers/CertifiedController.php
@@ -9,6 +9,26 @@ use Maatwebsite\Excel\Facades\Excel;
 class CertifiedController extends Controller
 {
     public function exportCertified($dni){
-        return Excel::download(new CertificadoExport($dni), 'FORMATO.xlsx');
+        // necesito que en el nombre salgo algo así FORMATO MESAÑO
+        $fecha = \Carbon\Carbon::now();
+        $numeroMes = $fecha->format('m');
+        $nombresMeses = [
+            '01' => 'ENERO',
+            '02' => 'FEBRERO',
+            '03' => 'MARZO',
+            '04' => 'ABRIL',
+            '05' => 'MAYO',
+            '06' => 'JUNIO',
+            '07' => 'JULIO',
+            '08' => 'AGOSTO',
+            '09' => 'SEPTIEMBRE',
+            '10' => 'OCTUBRE',
+            '11' => 'NOVIEMBRE',
+            '12' => 'DICIEMBRE',
+        ];
+        $nombreMes = $nombresMeses[$numeroMes];
+        $anio = $fecha->format('Y');
+        $nombreArchivo = 'FORMATO ' . $nombreMes . $anio . '.xlsx';
+        return Excel::download(new CertificadoExport($dni), $nombreArchivo);
     }
 }
