eaac661b9ed272afc5d8cdf5c8adeeee4d987a22
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-17 13:17:41 -0500

    Multiples sheets added to download

--- app/Http/Controllers/ProduccionController.php
+++ app/Http/Controllers/ProduccionController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Exports\ProduccionExport;
+use App\Exports\ResultadosExport;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ProduccionController extends Controller
 {
     public function exportProduccion(){
-        return Excel::download(new ProduccionExport, 'Producción diaria.xlsx');
+        return Excel::download(new ResultadosExport, 'Producción diaria.xlsx');
     }
 }
