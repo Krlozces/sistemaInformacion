33c9c0f62a7300e8166668b22cf9ae3169f3e265
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-17 15:32:45 -0500

    Styles to modal applied

--- resources/views/extraccion.blade.php
+++ resources/views/extraccion.blade.php
@@ -65,26 +65,30 @@
                     <span class="close" id="closePassword">&times;</span>
                 </div>
                 <!-- Modal body -->
-                <div class="p-4 md:p-5">
+                <div class="cambiar-password-form">
                     <form method="POST" class="space-y-4" action="{{ route('cambiar-password', ['id' => Auth::user()->id]) }}">
                         @csrf
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value={{ Auth::user()->email }} readonly />
+                        <div class="form-group">
+                            <label for="email"><i class="fa-solid fa-envelope"></i> Email</label>
+                            <input type="email" name="email" id="email" value={{ Auth::user()->email }} readonly />
+                            <i id="arroba" class="fa-solid fa-at"></i>
                         </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña actual</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        <div class="form-group">
+                            <label for="password" class="password-label"><i class="fas fa-lock"></i> Contraseña actual</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" required />
+                            <i class="fas fa-eye-slash" id="show-password1" onclick="togglePasswordVisibility1()"></i>
                         </div>
-                        <div>
-                            <label for="newPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nueva contraseña</label>
-                            <input type="password" name="newPassword" id="newPassword" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        <div class="form-group">
+                            <label for="newPassword"><i class="fas fa-lock"></i> Nueva contraseña</label>
+                            <input type="password" name="newPassword" id="newPassword" placeholder="••••••••" required />
+                            <i class="fas fa-eye-slash" id="show-password2" onclick="togglePasswordVisibility2()"></i>
                         </div>
-                        <div>
-                            <label for="confirmedPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmar contraseña</label>
-                            <input type="password" name="confirmedPassword" id="confirmedPassword" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        <div class="form-group">
+                            <label for="confirmedPassword"><i class="fas fa-lock"></i> Confirmar contraseña</label>
+                            <input type="password" name="confirmedPassword" id="confirmedPassword" placeholder="••••••••" required />
+                            <i class="fas fa-eye-slash" id="show-password3" onclick="togglePasswordVisibility3()"></i>
                         </div>
-                        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cambiar contraseña</button>    
+                        <button type="submit" class="bot">Cambiar contraseña</button>    
                     </form>
                 </div>
             </div>
