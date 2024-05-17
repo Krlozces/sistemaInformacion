156f32082c6d355ce61a385ddbb68506d4abc8f3
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-17 13:12:10 -0500

    Main page with multiple sheets created

---
+++ app/Exports/ResultadosExport.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Certificado;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+
+class ResultadosExport implements WithMultipleSheets
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function sheets(): array{
+        $sheets = [];
+        $sheets[] = new ProduccionExport();
+        $sheets[] = new ResultadosConsolidadosSheet();
+        $sheets[] = new ResultadosMotivosSheet();
+        $sheets[] = new ResultadosERSheet();
+        $sheets[] = new ResultadosNCMSheet();
+
+        return $sheets;
+    }
+}
