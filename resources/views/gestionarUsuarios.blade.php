453655aa70d4997d105311c14849b98aefc50bb6
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-21 16:12:06 -0500

    Modals to edit and delete users

--- resources/views/gestionarUsuarios.blade.php
+++ resources/views/gestionarUsuarios.blade.php
@@ -84,7 +84,39 @@
             </div>
         </div>
     </div>
-    
+
+    <div id="edit-modal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="direccion" id="closeEdit">
+                <h3>Editar</h3>
+                <span class="close">&times;</span>
+            </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5">
+                <form class="space-y-4" action="#">
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    </div>
+                    <div class="flex justify-between">
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                            </div>
+                            <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                        </div>
+                    </div>
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Editar</button>
+                </form>
+            </div>
+        </div>
+    </div> 
+
     <div id="myModal" class="modal">
         <div class="modal-content">
             <div class="direccion">
@@ -283,6 +315,11 @@
         var closeModal = document.getElementById("closeModal");
         var btnCancel = document.getElementById('btn-cancel');
 
+        var editModal = document.getElementById("edit-modal");
+        var btnEdit = document.getElementById("editar");
+        var closeEdit = document.getElementById("closeEdit");
+        var btnCancelEdit = document.getElementById('btn-cancel-edit');
+
         var savedImage = sessionStorage.getItem("selectedImage");
         if (savedImage) {
             mainImage.src = savedImage;
@@ -297,6 +334,10 @@
             popUpModal.style.display = "block";
         }
 
+        btnEdit.onclick = () => {
+            editModal.style.display = "block";
+        }
+
         // Cerrar el modal al hacer clic en la "x"
         span.onclick = function () {
             modal.style.display = "none";
@@ -306,15 +347,20 @@
             popUpModal.style.display = "none";
         }
 
+        closeEdit.onclick = () => {
+            editModal.style.display = "none";
+        }
+
         btnCancel.onclick=() => {
             popUpModal.style.display = "none";
         }
 
         // Cerrar el modal al hacer clic fuera del contenido del modal
         window.onclick = function (event) {
-            if (event.target == modal || event.target == popUpModal) {
+            if (event.target == modal || event.target == popUpModal || event.target == editModal) {
                 modal.style.display = "none";
                 popUpModal.style.display = "none";
+                editModal.style.display = "none";
             }
         }
 
@@ -347,8 +393,6 @@
                     previewImage.src = "";
                     mainImage.src = data.imageUrl;
                     sessionStorage.setItem("selectedImage", data.imageUrl);
-
-
                 })
                 .catch(error => {
                     console.error("Error al guardar la imagen:", error);
