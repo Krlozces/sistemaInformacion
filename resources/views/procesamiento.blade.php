f91a86cc31c668470696b70d0541fba4fe73151b
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-24 17:46:36 -0500

    JS fixed

--- resources/views/procesamiento.blade.php
+++ resources/views/procesamiento.blade.php
@@ -6,7 +6,6 @@
     <title>Área de Procesamiento</title>
     <link rel="icon" href="{{ asset('images/logo.png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="{{asset('js/importante.js')}}"></script>
     <link rel="stylesheet" href="{{ asset('css/procesamiento.css') }}">
 </head>
 <body>
@@ -354,6 +353,7 @@
                 <div class="Enviar">
                     <button type="reset">Cancelar</button>
                 </div>
+                <script src="{{ asset('js/importante.js') }}"></script>
             </div>
         </form>
     </div>
