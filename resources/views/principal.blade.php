c858100fcf8877dde47507cc28380dec9530c609
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-24 17:47:02 -0500

    JS fixed

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -6,8 +6,7 @@
     <title>Registrar</title>
     <link rel="icon" href="{{ asset('images/logo.png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="{{asset('js/importante.js')}}"></script>
-    <link rel="stylesheet" href="{{ asset('css/principal.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/principal.css') }}" />
 </head>
 
 <body>
@@ -211,7 +210,7 @@
                     <button type="reset">Cancelar</button>
                 </div>
             </div>
-
+            <script src="{{ asset('js/importante.js') }}"></script>
             </div>
         </form>
     </div>
