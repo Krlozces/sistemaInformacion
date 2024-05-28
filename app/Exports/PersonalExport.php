f254c794a8aa51674331f906ac9a61226eca19a6
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-27 23:42:50 -0500

    Data export with headings

--- app/Exports/PersonalExport.php
+++ app/Exports/PersonalExport.php
@@ -2,16 +2,31 @@
 
 namespace App\Exports;
 
-use App\Models\Personal;
+use App\Models\Persona;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class PersonalExport implements FromCollection
+class PersonalExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Personal::all();
+        return Persona::select('dni', 'nombre', 'apellido_paterno', 'apellido_materno', 'grado', 'telefono')
+        ->join('personal', 'personas.id', '=', 'personal.persona_id')
+        ->join('grados', 'personal.grado_id', '=', 'grados.id')
+        ->get();
     }
+
+    public function headings(): array{
+        return [
+            'DNI',
+            'Nombre',
+            'Apellido Paterno',
+            'Apellido Materno',
+            'Grado',
+            'Teléfono',
+        ];
+    } 
 }
