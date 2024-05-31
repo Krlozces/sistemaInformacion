31271fee739298e239d7a071b95c3efa1bf4d4f7
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-30 22:09:13 -0500

    Modals showing success or errors added

--- resources/views/tabla-certificados.blade.php
+++ resources/views/tabla-certificados.blade.php
@@ -12,7 +12,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="{{ asset('js/importante.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/busqueda.css') }}">
-    
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
 </head>
 
 <body>
@@ -112,14 +113,14 @@
             <h4>Filtro de Información</h4>
         </div>
         @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
+            <script>
+                swal("Buen trabajo!", "Registro exitoso!", "success");
+            </script>
         @endif
         @if (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
+            <script>
+                swal("Oooops!", "Ocurrió un error!", "error");
+            </script>
         @endif
         <div class="search-container">
             @csrf
