5102663ad973dd2a44bc00eb3fac2045599ff326
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-06-05 23:45:56 -0500

    Styles applied

--- app/Exports/PersonalExport.php
+++ app/Exports/PersonalExport.php
@@ -5,12 +5,16 @@ namespace App\Exports;
 use App\Models\Persona;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class PersonalExport implements FromCollection, WithHeadings
+class PersonalExport implements FromCollection, WithHeadings, WithStyles, WithEvents
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return Persona::select('dni', 'nombre', 'apellido_paterno', 'apellido_materno', 'grado', 'telefono')
@@ -19,14 +23,69 @@ class PersonalExport implements FromCollection, WithHeadings
         ->get();
     }
 
-    public function headings(): array{
+    public function headings(): array
+    {
         return [
             'DNI',
-            'Nombre',
+            'Nombres',
             'Apellido Paterno',
             'Apellido Materno',
             'Grado',
             'Teléfono',
         ];
-    } 
+    }
+
+    /**
+     * @param Worksheet $sheet
+     * @return array
+     */
+    public function styles(Worksheet $sheet)
+    {
+        // Ajustar el ancho de las columnas
+        $sheet->getColumnDimension('A')->setWidth(13);
+        $sheet->getColumnDimension('B')->setWidth(32);
+        $sheet->getColumnDimension('C')->setWidth(22);
+        $sheet->getColumnDimension('D')->setWidth(22);
+        $sheet->getColumnDimension('E')->setWidth(22);
+        $sheet->getColumnDimension('F')->setWidth(18);
+
+        // Centrando el contenido en las columnas A, E y F
+        $sheet->getStyle('A2:A' . $sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('E2:E' . $sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('F2:F' . $sheet->getHighestRow())->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+        // Centrando el encabezado
+        $sheet->getStyle('A1:F1')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+        return [
+            // Dar bordes a todo el rango de datos
+            'A1:F1' => [
+                'font' => ['bold' => true],
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                ],
+            ],
+            'A2:F' . $sheet->getHighestRow() => [
+                'borders' => [
+                    'allBorders' => [
+                        'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $event->sheet->getDelegate()->setAutoFilter('A1:F1');
+            },
+        ];
+    }
 }
