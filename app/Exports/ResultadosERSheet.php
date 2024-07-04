a5a86e21beece5a0ac2875ddcc1cce59e606d3ab
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-03 20:12:13 -0500

    Condition added to export only the registers of the month

--- app/Exports/ResultadosERSheet.php
+++ app/Exports/ResultadosERSheet.php
@@ -35,6 +35,7 @@ class ResultadosERSheet implements FromCollection, WithHeadings, WithStyles
         )
         ->join('muestras', 'muestras.id', '=', 'registros.muestra_id')
         ->where('muestras.descripcion', '!=', 'SIN MUESTRA')
+        ->whereRaw('DATE(registros.fecha_hora_extraccion) = CURDATE()')
         ->groupBy(DB::raw('DATE(registros.fecha_hora_extraccion)'))
         ->get();
     }
