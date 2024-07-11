8ac7323d72ada1ef948b3c85fb913651e8992dae
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-07-11 14:29:03 -0500

    Type of variable of doc de referencia changed

--- database/migrations/2024_03_22_153414_create_registros_table.php
+++ database/migrations/2024_03_22_153414_create_registros_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('registros', function (Blueprint $table) {
             $table->id();
-            $table->string('recepcion_doc_referencia')->unique();
-            $table->unsignedBigInteger('numero_oficio')->unique();
+            $table->string('recepcion_doc_referencia');
+            $table->string('numero_oficio')->unique();
             $table->dateTime('fecha_hora_infraccion');
             $table->dateTime('fecha_hora_extraccion');
             $table->unsignedBigInteger('extractor');
